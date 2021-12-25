5d38102a85f2fd1ef9f5ff35f31e12c5ec118209
sadraakhtarshenas <96003970+sadraakhtarshenas@users.noreply.github.com>
2021-12-25 05:23:26 -0800

    Add files via upload

---
+++ tamrin6.py
@@ -0,0 +1,9 @@
+import random
+while
+
+d = random.randint(1, 6)
+f = input("add ra vared khon")
+if f == 6:
+    print("Congratulations and you have a prize.")
+else:
+    print("Unfortunately you do not have a prize.")
